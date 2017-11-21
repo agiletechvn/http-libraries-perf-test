8e863fb8809049def01ff71a54cd4583ac675e7c
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-11-21 17:01:26 +0700

    first commit

--- index.js
+++ index.js
@@ -1,88 +1,122 @@
-var http = require('http');
-var axios = require('axios');
-var superagent = require('superagent');
-var request = require('request');
-
-var nock = require('nock');
-var HOST = 'test-perf';
+var http = require("http");
+var axios = require("axios");
+var superagent = require("superagent");
+var request = require("request");
+var fetch = require("node-fetch");
+var nock = require("nock");
+var HOST = "test-perf";
 
 axios.defaults.baseURL = `http://${HOST}`;
 
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite;
+const fetchURL = `http://${HOST}/test`;
 
-nock('http://test-perf').persist()
+var Benchmark = require("benchmark");
+var suite = new Benchmark.Suite();
+
+nock("http://test-perf")
+    .persist()
     // .log(console.log)
-    .post('/test').reply(200, 'ok')
-    .get('/test').reply(200, 'ok');
+    .post("/test")
+    .reply(200, "ok")
+    .get("/test")
+    .reply(200, "ok");
 
-suite.add('http.request POST request', {
+// http request
+suite.add("http.request POST request", {
     defer: true,
-    fn: (defer) => {
-        var req = http.request({ host: HOST, path: '/test', method: 'POST' }, (res) => {
-            res.resume().on('end', () => defer.resolve());
-        });
+    fn: defer => {
+        var req = http.request(
+            { host: HOST, path: "/test", method: "POST" },
+            res => {
+                res.resume().on("end", () => defer.resolve());
+            }
+        );
         req.write();
         req.end();
     }
 });
 
-suite.add('http.request GET request', {
+suite.add("http.request GET request", {
     defer: true,
-    fn: (defer) => {
-        http.request({ path: '/test', host: HOST }, (res) => {
-            res.resume().on('end', () => defer.resolve());
-        }).end();
+    fn: defer => {
+        http
+            .request({ path: "/test", host: HOST }, res => {
+                res.resume().on("end", () => defer.resolve());
+            })
+            .end();
     }
 });
 
-suite.add('axios GET request', {
+// node-fetch
+suite.add("node-fetch POST request", {
     defer: true,
-    fn: (defer) => {
-        axios.get('/test').then(() => defer.resolve())
+    fn: defer => {
+        fetch(fetchURL, { method: "POST" }).then(() => defer.resolve());
     }
 });
 
-suite.add('axios POST request', {
+suite.add("node-fetch GET request", {
     defer: true,
-    fn: (defer) => {
-        axios.post('/test').then(() => defer.resolve());
+    fn: defer => {
+        fetch(fetchURL).then(() => defer.resolve());
     }
 });
 
-suite.add('superagent GET request', {
+// axios
+suite.add("axios GET request", {
     defer: true,
-    fn: (defer) => {
-        superagent.get(`http://${HOST}/test`).end(() => { defer.resolve(); });
+    fn: defer => {
+        axios.get("/test").then(() => defer.resolve());
     }
 });
 
-suite.add('superagent POST request', {
+suite.add("axios POST request", {
     defer: true,
-    fn: (defer) => {
-        superagent.post(`http://${HOST}/test`).send().end(() => defer.resolve());
+    fn: defer => {
+        axios.post("/test").then(() => defer.resolve());
     }
 });
 
-suite.add('Request GET request', {
+// superagent
+suite.add("superagent GET request", {
     defer: true,
-    fn: (defer) => {
+    fn: defer => {
+        superagent.get(`http://${HOST}/test`).end(() => {
+            defer.resolve();
+        });
+    }
+});
+
+suite.add("superagent POST request", {
+    defer: true,
+    fn: defer => {
+        superagent
+            .post(`http://${HOST}/test`)
+            .send()
+            .end(() => defer.resolve());
+    }
+});
+
+// request
+suite.add("Request GET request", {
+    defer: true,
+    fn: defer => {
         request(`http://${HOST}/test`, () => defer.resolve());
     }
 });
 
-suite.add('Request POST request', {
+suite.add("Request POST request", {
     defer: true,
-    fn: (defer) => {
+    fn: defer => {
         request.post({ url: `http://${HOST}/test` }, () => defer.resolve());
     }
 });
 
-suite.on('complete', function(defer) {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
+suite.on("complete", function(defer) {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
 });
 
-suite.on('cycle', function(event) {
+suite.on("cycle", function(event) {
     console.log(String(event.target));
 });
 
